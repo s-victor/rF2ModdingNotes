d5cde2e56518c00e97e91e14767c5c96b98a19e3
Xiang <flyinwhite@outlook.com>
2025-08-02 10:34:49 +0800

    use raw path for images, set custom export path

--- export.sh
+++ export.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-echo "Parse and export markdown file for github wiki"
+echo -e "Parse and export markdown file for github wiki\n"
+
+# Set export path
+if [ -n "$1" ]
+then
+    # export path from argument
+    FOLDER_PATH="$1"
+else
+    # default path
+    FOLDER_PATH="export"
+fi
 
 # Create export folder if not exist
-mkdir -p "export"
+mkdir -p ${FOLDER_PATH}
 
-# Get repo path
-PATH_REPO="$( git config --get remote.origin.url | sed 's/rF2ModdingNotes.git/rF2ModdingNotes/' )"
-echo -e "REPO PATH: ${PATH_REPO}\n"
+# Get repo raw content path
+PATH_REPO="$( git config --get remote.origin.url | sed 's/github.com/raw.githubusercontent.com/' | sed 's/rF2ModdingNotes.git/rF2ModdingNotes/' )"
+echo -e "REPO RAW PATH: ${PATH_REPO}\n"
 
 # Set & replace image path
 PATH_REL=$(echo "../images/" | sed 's/\//\\\//g')
-PATH_ABS=$(echo "${PATH_REPO}/raw/master/images/" | sed 's/\//\\\//g')
+PATH_ABS=$(echo "${PATH_REPO}/master/images/" | sed 's/\//\\\//g')
 
-# Export files to ./export folder
+# Export files to export folder
 for SOURCE_MD in src/*.md;
 do
     echo "SOURCE: ${SOURCE_MD}"
-    EXPORT_PATH=$(echo "./export/${SOURCE_MD}" | sed 's/src\///')
+    EXPORT_PATH=$(echo "${FOLDER_PATH}/${SOURCE_MD}" | sed 's/src\///')
     cp ${SOURCE_MD} ${EXPORT_PATH}
     sed -i "s/$PATH_REL/$PATH_ABS/" ${EXPORT_PATH}
     echo -e "EXPORT: ${EXPORT_PATH}\n"
